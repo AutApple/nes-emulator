b24fa3239afd5fc76e3647cbbc5fb962001e154e
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-03-25 21:08:47 +0300

    implement cpu class prototype

---
+++ NESCpu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NESEmulator
+{
+    internal class NESCpu(NESMemoryBus bus)
+    {
+        public byte A { get; private set; } // a register
+        public byte X { get; private set; } // x register
+        public byte Y { get; private set; } // y register
+
+        public ushort PC { get; private set; } // 2 byte program counter 
+
+        public byte S { get; private set; } // stack pointer
+        public byte P { get; private set; } // status register
+        private NESMemoryBus _bus = bus;
+    }
+}
