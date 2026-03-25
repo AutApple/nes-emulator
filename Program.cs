8781d10ece4ae7cdf1f69bc0b4305adb095cd637
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-03-25 21:26:42 +0300

    add test code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// https://github.com/nwidger/nintengo/blob/master/m65go2/test-roms/nestest/nestest.log
+using NESEmulator;
+
+string path = args[0] ?? "test.nes";
+
+NESMemoryBus bus = new NESMemoryBus();
+NESRom rom = new NESRom(bus);
+NESCpu cpu = new NESCpu(bus);
+rom.Load(path);
+
+Console.WriteLine("Successfully loaded ROM into memory!");
